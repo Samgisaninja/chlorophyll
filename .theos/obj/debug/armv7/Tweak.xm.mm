c47b20949c0d3291e1c7d934afcc7c2d4a62b579
Samgisaninja <stgardner4@att.net>
2019-05-17 00:52:31 -0500

    merge calico

--- .theos/obj/debug/armv7/Tweak.xm.mm
+++ .theos/obj/debug/armv7/Tweak.xm.mm
@@ -1,4 +1,56 @@
 #line 1 "Tweak.xm"
+#import <UIKit/UIKit.h>
+
+static BOOL backgroundColor = YES;
+static BOOL outlineColor = YES;
+static BOOL textColor = YES;
+
+static UIColor *customBackgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
+static UIColor *customOutlineColor = [UIColor colorWithRed:0.0 green:0.80 blue:0.35 alpha:1.00];
+static UIColor *customTextColor = [UIColor colorWithRed:0.0 green:0.80 blue:0.35 alpha:1.00];
+
+
+#define kIdentifier @"com.samgisaninja.chlorophyll"
+#define kSettingsChangedNotification (CFStringRef)@"com.samgisaninja.chlorophyll/settingschanged"
+#define kSettingsPath @"/var/mobile/Library/Preferences/com.samgisaninja.chlorophyll.plist"
+
+
+
+NSDictionary *prefs = nil;
+
+static void reloadPrefs() {
+	if ([NSHomeDirectory() isEqualToString:@"/var/mobile"]) {
+		CFArrayRef keyList = CFPreferencesCopyKeyList((CFStringRef)kIdentifier, kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
+		if (keyList) {
+			prefs = (NSDictionary *)CFPreferencesCopyMultiple(keyList, (CFStringRef)kIdentifier, kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
+			if (!prefs) {
+				prefs = [NSDictionary new];
+			}
+			CFRelease(keyList);
+		}
+	} else {
+		prefs = [NSDictionary dictionaryWithContentsOfFile:kSettingsPath];
+	}
+}
+
+static BOOL boolValueForKey(NSString *key, BOOL defaultValue) {
+	return (prefs && [prefs objectForKey:key]) ? [[prefs objectForKey:key] boolValue] : defaultValue;
+}
+
+
+static void preferencesChanged() {
+	CFPreferencesAppSynchronize((CFStringRef)kIdentifier);
+	reloadPrefs();
+
+	 	backgroundColor = boolValueForKey(@"backgroundColor", YES);
+		outlineColor = boolValueForKey(@"outline", YES);
+	 	textColor = boolValueForKey(@"textColor", YES);
+}
+
+@interface AppStoreButtonLabel : UILabel
+-(void)setVariablesForCurrentLabel;
+@end
+
 
 #include <substrate.h>
 #if defined(__clang__)
@@ -20,63 +72,91 @@
 #define _LOGOS_RETURN_RETAINED
 #endif
 
-@class UILabel; 
-static id (*_logos_orig$_ungrouped$UILabel$font)(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL); static id _logos_method$_ungrouped$UILabel$font(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL); static UILabel* (*_logos_orig$_ungrouped$UILabel$initWithCoder$)(_LOGOS_SELF_TYPE_INIT UILabel*, SEL, id) _LOGOS_RETURN_RETAINED; static UILabel* _logos_method$_ungrouped$UILabel$initWithCoder$(_LOGOS_SELF_TYPE_INIT UILabel*, SEL, id) _LOGOS_RETURN_RETAINED; static UILabel* (*_logos_orig$_ungrouped$UILabel$initWithFrame$)(_LOGOS_SELF_TYPE_INIT UILabel*, SEL, CGRect) _LOGOS_RETURN_RETAINED; static UILabel* _logos_method$_ungrouped$UILabel$initWithFrame$(_LOGOS_SELF_TYPE_INIT UILabel*, SEL, CGRect) _LOGOS_RETURN_RETAINED; static void (*_logos_orig$_ungrouped$UILabel$setFont$)(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$UILabel$setFont$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, id); static void (*_logos_orig$_ungrouped$UILabel$setWantsFastBaselineMeasurement$)(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$UILabel$setWantsFastBaselineMeasurement$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, BOOL); static void (*_logos_orig$_ungrouped$UILabel$traitCollectionDidChange$)(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$UILabel$traitCollectionDidChange$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL, id); static BOOL (*_logos_orig$_ungrouped$UILabel$wantsFastBaselineMeasurement)(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL); static BOOL _logos_method$_ungrouped$UILabel$wantsFastBaselineMeasurement(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST, SEL); 
+@class AppStoreButtonLabel; 
+static void _logos_method$_ungrouped$AppStoreButtonLabel$setVariablesForCurrentLabel(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL); static id (*_logos_orig$_ungrouped$AppStoreButtonLabel$font)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL); static id _logos_method$_ungrouped$AppStoreButtonLabel$font(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL); static id (*_logos_orig$_ungrouped$AppStoreButtonLabel$initWithCoder$)(_LOGOS_SELF_TYPE_INIT id, SEL, id) _LOGOS_RETURN_RETAINED; static id _logos_method$_ungrouped$AppStoreButtonLabel$initWithCoder$(_LOGOS_SELF_TYPE_INIT id, SEL, id) _LOGOS_RETURN_RETAINED; static id (*_logos_orig$_ungrouped$AppStoreButtonLabel$initWithFrame$)(_LOGOS_SELF_TYPE_INIT id, SEL, CGRect) _LOGOS_RETURN_RETAINED; static id _logos_method$_ungrouped$AppStoreButtonLabel$initWithFrame$(_LOGOS_SELF_TYPE_INIT id, SEL, CGRect) _LOGOS_RETURN_RETAINED; static void (*_logos_orig$_ungrouped$AppStoreButtonLabel$setFont$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$AppStoreButtonLabel$setFont$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void (*_logos_orig$_ungrouped$AppStoreButtonLabel$_setTextColor$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$AppStoreButtonLabel$_setTextColor$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void (*_logos_orig$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, BOOL); static void (*_logos_orig$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static BOOL (*_logos_orig$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL); static BOOL _logos_method$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL); 
 
-#line 1 "Tweak.xm"
+#line 53 "Tweak.xm"
 
 
-static id _logos_method$_ungrouped$UILabel$font(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
-	return _logos_orig$_ungrouped$UILabel$font(self, _cmd);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+
+static void _logos_method$_ungrouped$AppStoreButtonLabel$setVariablesForCurrentLabel(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
 	
-}
 
-static UILabel* _logos_method$_ungrouped$UILabel$initWithCoder$(_LOGOS_SELF_TYPE_INIT UILabel* __unused self, SEL __unused _cmd, id arg1) _LOGOS_RETURN_RETAINED{
-	return _logos_orig$_ungrouped$UILabel$initWithCoder$(self, _cmd, arg1);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
+
+
+
+	UILabel *label = (UILabel *)self;
+	if ([label.text isEqualToString:@"OPEN"]){
+		UIColor *customBackgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
+		UIColor *customOutlineColor = [UIColor colorWithRed:0.0 green:0.80 blue:0.35 alpha:1.00];
+		if (backgroundColor || outlineColor) {
+			label.layer.cornerRadius = 15.0;
+		}
+		if (backgroundColor){
+			label.layer.backgroundColor = [customBackgroundColor CGColor];
+		}
+		if (outlineColor) {
+			label.layer.borderColor = [customOutlineColor CGColor];
+			label.layer.borderWidth = 0.5;
+		}
+
 	}
+	self = label;
+	return;
 }
 
-static UILabel* _logos_method$_ungrouped$UILabel$initWithFrame$(_LOGOS_SELF_TYPE_INIT UILabel* __unused self, SEL __unused _cmd, CGRect arg1) _LOGOS_RETURN_RETAINED{
-	return _logos_orig$_ungrouped$UILabel$initWithFrame$(self, _cmd, arg1);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+static id _logos_method$_ungrouped$AppStoreButtonLabel$font(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
+	return _logos_orig$_ungrouped$AppStoreButtonLabel$font(self, _cmd);
+	[self setVariablesForCurrentLabel];
+
 }
 
-static void _logos_method$_ungrouped$UILabel$setFont$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
-	_logos_orig$_ungrouped$UILabel$setFont$(self, _cmd, arg1);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+static id _logos_method$_ungrouped$AppStoreButtonLabel$initWithCoder$(_LOGOS_SELF_TYPE_INIT id __unused self, SEL __unused _cmd, id arg1) _LOGOS_RETURN_RETAINED{
+	return _logos_orig$_ungrouped$AppStoreButtonLabel$initWithCoder$(self, _cmd, arg1);
+	[self setVariablesForCurrentLabel];
 }
 
-static void _logos_method$_ungrouped$UILabel$setWantsFastBaselineMeasurement$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, BOOL arg1){
-	_logos_orig$_ungrouped$UILabel$setWantsFastBaselineMeasurement$(self, _cmd, arg1);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+static id _logos_method$_ungrouped$AppStoreButtonLabel$initWithFrame$(_LOGOS_SELF_TYPE_INIT id __unused self, SEL __unused _cmd, CGRect arg1) _LOGOS_RETURN_RETAINED{
+	return _logos_orig$_ungrouped$AppStoreButtonLabel$initWithFrame$(self, _cmd, arg1);
+	[self setVariablesForCurrentLabel];
 }
 
-static void _logos_method$_ungrouped$UILabel$traitCollectionDidChange$(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
-	_logos_orig$_ungrouped$UILabel$traitCollectionDidChange$(self, _cmd, arg1);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+static void _logos_method$_ungrouped$AppStoreButtonLabel$setFont$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
+	_logos_orig$_ungrouped$AppStoreButtonLabel$setFont$(self, _cmd, arg1);
+	[self setVariablesForCurrentLabel];
 }
 
-static BOOL _logos_method$_ungrouped$UILabel$wantsFastBaselineMeasurement(_LOGOS_SELF_TYPE_NORMAL UILabel* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
-	return _logos_orig$_ungrouped$UILabel$wantsFastBaselineMeasurement(self, _cmd);
-	if ([[self text] isEqualToString:@"OPEN"]){
-		self.backgroundColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:0.25];
-	}
+static void _logos_method$_ungrouped$AppStoreButtonLabel$_setTextColor$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
+  UILabel *_ = (UILabel *)self;
+	UIColor *customTextColor = [UIColor colorWithRed:0.0 green:0.80 blue:0.35 alpha:1.00];
+  if (textColor && [_.text isEqual:@"OPEN"]) {
+      _logos_orig$_ungrouped$AppStoreButtonLabel$_setTextColor$(self, _cmd, customTextColor);
+  } else {
+    _logos_orig$_ungrouped$AppStoreButtonLabel$_setTextColor$(self, _cmd, arg1);
+  }
+}
+
+static void _logos_method$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, BOOL arg1){
+	_logos_orig$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$(self, _cmd, arg1);
+	[self setVariablesForCurrentLabel];
+}
+
+static void _logos_method$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
+	_logos_orig$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$(self, _cmd, arg1);
+	[self setVariablesForCurrentLabel];
+}
+
+static BOOL _logos_method$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
+	return _logos_orig$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement(self, _cmd);
+	[self setVariablesForCurrentLabel];
 }
 
 
-static __attribute__((constructor)) void _logosLocalInit() {
-{Class _logos_class$_ungrouped$UILabel = objc_getClass("UILabel"); MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(font), (IMP)&_logos_method$_ungrouped$UILabel$font, (IMP*)&_logos_orig$_ungrouped$UILabel$font);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(initWithCoder:), (IMP)&_logos_method$_ungrouped$UILabel$initWithCoder$, (IMP*)&_logos_orig$_ungrouped$UILabel$initWithCoder$);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(initWithFrame:), (IMP)&_logos_method$_ungrouped$UILabel$initWithFrame$, (IMP*)&_logos_orig$_ungrouped$UILabel$initWithFrame$);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(setFont:), (IMP)&_logos_method$_ungrouped$UILabel$setFont$, (IMP*)&_logos_orig$_ungrouped$UILabel$setFont$);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(setWantsFastBaselineMeasurement:), (IMP)&_logos_method$_ungrouped$UILabel$setWantsFastBaselineMeasurement$, (IMP*)&_logos_orig$_ungrouped$UILabel$setWantsFastBaselineMeasurement$);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(traitCollectionDidChange:), (IMP)&_logos_method$_ungrouped$UILabel$traitCollectionDidChange$, (IMP*)&_logos_orig$_ungrouped$UILabel$traitCollectionDidChange$);MSHookMessageEx(_logos_class$_ungrouped$UILabel, @selector(wantsFastBaselineMeasurement), (IMP)&_logos_method$_ungrouped$UILabel$wantsFastBaselineMeasurement, (IMP*)&_logos_orig$_ungrouped$UILabel$wantsFastBaselineMeasurement);} }
-#line 54 "Tweak.xm"
+
+
+static __attribute__((constructor)) void _logosLocalCtor_e6244da5(int __unused argc, char __unused **argv, char __unused **envp) {
+	preferencesChanged();
+  {Class _logos_class$_ungrouped$AppStoreButtonLabel = NSClassFromString(@"AppStore.DynamicTypeLabel"); { char _typeEncoding[1024]; unsigned int i = 0; _typeEncoding[i] = 'v'; i += 1; _typeEncoding[i] = '@'; i += 1; _typeEncoding[i] = ':'; i += 1; _typeEncoding[i] = '\0'; class_addMethod(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(setVariablesForCurrentLabel), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$setVariablesForCurrentLabel, _typeEncoding); }MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(font), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$font, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$font);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(initWithCoder:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$initWithCoder$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$initWithCoder$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(initWithFrame:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$initWithFrame$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$initWithFrame$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(setFont:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$setFont$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$setFont$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(_setTextColor:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$_setTextColor$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$_setTextColor$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(setWantsFastBaselineMeasurement:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$setWantsFastBaselineMeasurement$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(traitCollectionDidChange:), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$traitCollectionDidChange$);MSHookMessageEx(_logos_class$_ungrouped$AppStoreButtonLabel, @selector(wantsFastBaselineMeasurement), (IMP)&_logos_method$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement, (IMP*)&_logos_orig$_ungrouped$AppStoreButtonLabel$wantsFastBaselineMeasurement);}
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)preferencesChanged, kSettingsChangedNotification, NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+
+}
